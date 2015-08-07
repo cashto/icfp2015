a015dc8891bceafcfa1f0389b666e3134e6c0576
cashto <cashto@gmail.com>
2015-08-07 16:36:00 -0700

    Add PriorityQueue (adapted from previous years ICFP submission)

--- solve/Program.cs
+++ solve/Program.cs
@@ -15,6 +15,71 @@ static class Constants
 }
 
 
+class PriorityQueue<T>
+{
+    PriorityQueue(Func<T, T, bool> lessFn)
+    {
+        this.lessFn = lessFn;
+    }
+
+    public void push(T obj)
+    {
+        items.Add(obj);
+        var i = items.Count - 1;
+
+        while (lessFn(items[i / 2], items[i]))
+        {
+            var j = i / 2;
+            swap(i, j);
+            i = j;
+        }
+    }
+
+    public T pop()
+    {
+        var i = 0;
+        swap(i, items.Count - 1);
+
+        T obj = items.Last();
+        items.RemoveAt(items.Count - 1);
+
+        while (true)
+        {
+            var largest = i;
+
+            if (i * 2 < items.Count && lessFn(items[largest], items[i * 2]))
+            {
+                largest = i * 2;
+            }
+
+            if (i * 2 + 1 < items.Count && lessFn(items[largest], items[i * 2 + 1]))
+            {
+                largest = i * 2 + 1;
+            }
+
+            if (i == largest)
+            {
+                return obj;
+            }
+
+            swap(i, largest);
+
+            i = largest;
+        }
+    }
+
+    private void swap(int i, int j)
+    {
+        T t = items[i];
+        items[i] = items[j];
+        items[j] = t;
+    }
+
+    private Func<T, T, bool> lessFn;
+    private List<T> items;
+}
+
+
 public class Random
 {
     public Random(UInt32 seed)
